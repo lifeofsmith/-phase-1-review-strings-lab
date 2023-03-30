8e71d0182eaaba857cb7ada5ef60b5bb5435d880
ian smith <smithkarim41@gmail.com>
2023-03-30 16:23:51 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+//const welcomeMessage = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, + Grace Hopper!';
+const excitedWelcomeMessage = ('Welcome to Flatbook, + Grace Hopper!'.toUpperCase)();
+const shortGreeting = `Welcome, ${'Grace Hopper'.slice(0,1)}!`; 
